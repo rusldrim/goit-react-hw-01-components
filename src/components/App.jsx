4ed937ef853683c6ffb8f81f2b75c625755ec635
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-05-29 12:45:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import User from './User/User';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import Statistics from './Statistics/Statistics';
+
+import data from 'path/data.json';
+import user from 'path/user.json';
+import friends from 'path/friends.json';
+import transactions from 'path/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <User
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList stats={friends} />
+      <TransactionHistory stats={transactions} />
     </div>
   );
 };
