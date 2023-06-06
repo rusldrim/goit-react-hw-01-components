f49bef1cd88317bf2916e1b752104cbc5599738b
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-06-06 16:38:56 +0300

    Update User.jsx

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -41,7 +41,9 @@ User.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
-}
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
