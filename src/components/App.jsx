f77b1e834557a2afac997b858d72271259f69359
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-06-07 21:37:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,37 @@
-import User from './User/User';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import Statistics from './Statistics/Statistics';
-
-import data from 'path/data.json';
-import user from 'path/user.json';
-import friends from 'path/friends.json';
-import transactions from 'path/transactions.json';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './Friends/friends.json';
+import transactions from './Transactions/transactions.json';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './Friends/FriendList';
+import { TransactionHistory } from './Transactions/TransactionsHistory';
 
 export const App = () => {
   return (
-    <div>
-      <User
-        avatar={user.avatar}
+    <div
+      style={{
+        
+        padding: 30,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
-        stats={user.stats} 
+        avatar={user.avatar}
+        stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList stats={friends} />
-      <TransactionHistory stats={transactions} />
+      <Statistics title={"Upload stats"} stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
