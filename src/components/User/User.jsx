b7d9e92c79037f9b97068743de2172a31825cee4
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-06-09 10:13:39 +0300

    Create User.jsx

---
+++ src/components/User/User.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+import css from './User.module.css';
+export default function User({
+  avatar,
+  username,
+  tag,
+  location,
+  followers,
+  views,
+  likes,
+}) {
+  return (
+    <div className={css.profile}>
+      <div className={css.profile__description}>
+        <img src={avatar} alt={username} className={css.profile__avatar} />
+        <p className={css.profile__name}>{username}</p>
+        <p className={css.profile__tag}>@{tag}</p>
+        <p className={css.profile__location}>{location}</p>
+      </div>
+
+      <ul className={css.profile__stats}>
+        <li className={css.profile__statsItem}>
+          <span className={css.profile__label}>Followers</span>
+          <span className={css.profile__quantity}>{followers}</span>
+        </li>
+        <li className={css.profile__statsItem}>
+          <span className={css.profile__label}>Views</span>
+          <span className={css.profile__quantity}>{views}</span>
+        </li>
+        <li className={css.profile__statsItem}>
+          <span className={css.profile__label}>Likes</span>
+          <span className={css.profile__quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+User.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
